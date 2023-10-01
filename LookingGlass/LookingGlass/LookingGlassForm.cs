41faf63ef1432ed592fcbe389225ffa4c63b9fcf
achippyland <cheung.alan94@gmail.com>
2023-09-30 20:34:08 -0400

    Set transparency

--- LookingGlass/LookingGlass/LookingGlassForm.cs
+++ LookingGlass/LookingGlass/LookingGlassForm.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace LookingGlass
 {
     public partial class LookingGlassForm : Form
     {
+        private const int DEFAULT_WIDTH = 1920;
+        private const int DEFAULT_HEIGHT = 1080;
+
+        private bool IsTransparent = true;
+
         public LookingGlassForm()
         {
             InitializeComponent();
+
+            // Add the window borders to the overall size of the app
+            // The viewBox is set to fill the space.
+            int widthDelta = DEFAULT_WIDTH - this.viewBox.Width;
+            int heightDelta = DEFAULT_HEIGHT - this.viewBox.Height;
+            this.Width += widthDelta;
+            this.Height += heightDelta;
+
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = Color.Transparent;
+            this.TransparencyKey = Color.Transparent;
+            this.viewBox.Visible = false;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            if (!IsTransparent)
+            {
+                base.OnPaintBackground(e);
+            }
         }
     }
 }
