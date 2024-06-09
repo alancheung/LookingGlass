e19d8a0582a823c15db23376d3e19d9b54b720bc
achippyland <cheung.alan94@gmail.com>
2024-06-08 21:36:24 -0400

    Initial commit

--- LookingGlass/LookingGlass.CPP/Resource.h
+++ LookingGlass/LookingGlass.CPP/Resource.h
@@ -1,17 +1,17 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by LookingGlass.rc
+// Used by LookingGlass.CPP.rc
 
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_LOOKINGGLASS_DIALOG	102
+#define IDD_LOOKINGGLASSCPP_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_LOOKINGGLASS			107
+#define IDI_LOOKINGGLASSCPP			107
 #define IDI_SMALL				108
-#define IDC_LOOKINGGLASS			109
+#define IDC_LOOKINGGLASSCPP			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
